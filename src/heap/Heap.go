576ff12c84b8ef3e144e39ff0c14584185118503
lukajose <lukajosegamulin@outlook.com>
2021-08-26 18:43:10 +1000

    adding new heap nodemax comparable to heap, finishing heapifyup and heapifydown

--- src/heap/Heap.go
+++ src/heap/Heap.go
@@ -1,15 +1,44 @@
 package heap
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Comparable interface {
-	CompareTo(interface{}) int
+	CompareTo(item interface{}) (int, error)
 }
 
 type Heap struct {
 	items    []Comparable
 	capacity int
-	next     int
+	size     int
+}
+
+type NodeMax struct {
+	item int
+}
+
+func max(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func (n *NodeMax) CompareTo(item interface{}) (int, error) {
+	val, ok := item.(int)
+	if ok {
+		switch true {
+		case n.item > val:
+			return 1, nil
+		case n.item < val:
+			return -1, nil
+		default:
+			return 0, nil
+		}
+	}
+	return -1, fmt.Errorf("Invalid type comparison")
 }
 
 /*
@@ -19,6 +48,7 @@ func NewHeap(capacity int) *Heap {
 	return &Heap{
 		items:    make([]Comparable, 0, capacity),
 		capacity: capacity,
+		size:     0,
 	}
 }
 
@@ -30,11 +60,15 @@ func (h *Heap) resize() {
 }
 
 func (h *Heap) heapifyUp() {
-	cIndex := len(h.items) - 1
+	if h.size == 1 {
+		return
+	}
+	cIndex := h.size - 1
 	child := h.items[cIndex]
 	for cIndex >= 0 {
 		pIndex := int(math.Floor(float64(cIndex) / 2))
-		if child.CompareTo(h.items[pIndex]) > 0 {
+		greater, ok := child.CompareTo(h.items[pIndex])
+		if ok != nil && greater > 0 {
 			// swap if element child is greater than parent
 			h.items[pIndex], h.items[cIndex] = h.items[cIndex], h.items[pIndex]
 		}
@@ -44,25 +78,69 @@ func (h *Heap) heapifyUp() {
 
 }
 
+func (h *Heap) getMaxChild(parent int) (int, error) {
+	c1, c2 := (parent * 2), (parent*2)+1
+	if c1 > h.size && c2 > h.size {
+		return -1, fmt.Errorf("both childs are out of range")
+	}
+	if c1 > h.size && c2 < h.size {
+		return c1, nil
+	}
+	if c1 < h.size && c2 > h.size {
+		return c2, nil
+	}
+	greater, err := h.items[c1].CompareTo(h.items[c2])
+	if err != nil {
+		return -1, err
+	}
+	if greater > 0 {
+		return c1, nil
+	}
+	return c2, nil
+
+}
+
 func (h *Heap) heapifyDown() {
+	if h.size <= 1 {
+		return
+	}
+	parent := 0
+	for parent < h.size {
+		// get max of child nodes
+		c, ok := h.getMaxChild(parent)
+		if ok != nil {
+			break
+		}
+		// if parent is smaller than child swap
+		lower, ok := h.items[parent].CompareTo(h.items[c])
+		if lower < 0 && ok != nil {
+			// swap items
+			h.items[parent], h.items[c] = h.items[c], h.items[parent]
+		}
+		parent++
+
+	}
 
 }
 
 func (h *Heap) Add(item Comparable) {
-	if h.next > h.capacity {
+	if h.size == h.capacity {
 		h.resize()
 	}
-	h.items[len(h.items)-1] = item
+	h.items[h.size] = item
+	h.size++
 	h.heapifyUp()
+
 }
 
 func (h *Heap) Pop() Comparable {
 	if len(h.items) == 0 {
 		return nil
 	}
-	lindex := len(h.items) - 1
+	lindex := h.size - 1
 	last := h.items[lindex]
 	h.items = h.items[:lindex]
 	h.heapifyDown()
+	h.size = max(h.size-1, 0)
 	return last
 }
---
+++ src/heap/heap_test.go
@@ -0,0 +1,12 @@
+package heap
+
+import "testing"
+
+func TestHeap(t *testing.T) {
+	h := Heap{}
+	n1 := NodeMax{item: 5}
+	n2 := NodeMax{item: 10}
+	h.Add(n1)
+	h.Add(n2)
+
+}
